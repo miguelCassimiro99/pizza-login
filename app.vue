2125f6751cc0d9b8f1502b31132c591cef223821
Miguel Cassimiro <miguel.cassimiro99@gmail.com>
2022-09-07 13:30:51 -0300

    added pug and color mode

--- app.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="bg-black text-green-700">
-    Hello world!
-  </div>
-</template>
--- pages/login.vue
+++ pages/login.vue
@@ -1,3 +1,19 @@
-<template>
-  
+<template lang="pug">
+main(class="h-screen dark:bg-slate-900")
+  .login-container(class="h-[480px] w-[768px] rounded-lg shadow-xl")
+    form.form
+    form.form
 </template>
+
+<script setup lang="ts">
+  type Theme = 'light' | 'dark';
+
+  const setColorTheme = (newTheme: Theme) => {
+    useColorMode().preference = newTheme;
+  }
+
+  onMounted(() => {
+    setColorTheme('dark')
+  });
+
+</script>
