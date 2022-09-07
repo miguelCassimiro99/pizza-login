523457c35633e18c2151092a4695826315d42458
Miguel Cassimiro <miguel.cassimiro99@gmail.com>
2022-09-07 16:43:06 -0300

    created animation on different forms

--- pages/login.vue
+++ pages/login.vue
@@ -1,13 +1,25 @@
 <template lang="pug">
-main(class="h-screen dark:bg-slate-900")
-  .login-container(class="h-[480px] w-[768px] rounded-lg shadow-xl")
-    form.form
-    form.form
+main(class="h-screen dark:bg-slate-900 flex justify-center items-center")
+  .login-container(:class="formSelected ? 'toggle h-[480px] w-[768px] rounded-md shadow-xl overflow-hidden border-slate-900 relative' : ' h-[480px] w-[768px] rounded-md shadow-xl overflow-hidden border-slate-900 relative'")
+    .form-container(class="h-full flex")
+      form.form(class="h-full w-[50%]")
+        h2(class="text-xl text-orange-600") Entrar com
+
+      form.form(class="h-full w-[50%]")
+        h2(class="text-xl text-orange-600") Cadastrar
+
+    .form-overlay(class="w-[50%] h-full absolute top-0 left-0 transition-all")
+      .overlay-one(class="absolute w-full h-full bg-orange-600" v-if="formSelected")
+        button(@click="toggleForm()") Já tenho conta
+      .overlay-two(class="absolute w-full h-full bg-orange-600" v-else)
+        button(@click="toggleForm()") cadastrar
 </template>
 
 <script setup lang="ts">
   type Theme = 'light' | 'dark';
 
+  const formSelected = ref(false);
+ 
   const setColorTheme = (newTheme: Theme) => {
     useColorMode().preference = newTheme;
   }
@@ -15,5 +27,9 @@ main(class="h-screen dark:bg-slate-900")
   onMounted(() => {
     setColorTheme('dark')
   });
+
+  const toggleForm = () => {
+    return formSelected.value = !formSelected.value;
+  }
 
 </script>
