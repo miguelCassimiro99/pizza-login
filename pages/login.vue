ded709efafe49a380cb6b68788eff6bf5f80feb8
Miguel Cassimiro <miguel.cassimiro99@gmail.com>
2022-09-10 17:12:54 -0300

    added responsive mode

--- pages/login.vue
+++ pages/login.vue
@@ -1,12 +1,32 @@
 <template lang="pug">
 main(class="h-screen dark:bg-slate-900 flex justify-center items-center")
-  .login-container(:class="formSelected ? 'toggle h-[480px] w-[768px] rounded-md shadow-xl overflow-hidden border-slate-900 relative' : ' h-[480px] w-[768px] rounded-md shadow-xl overflow-hidden border-slate-900 relative'")
+  .login-container(
+    class="h-full w-full md:h-[480px] md:w-[768px] rounded-md shadow-none md:shadow-xl overflow-hidden border-slate-900 relative"
+    :class="formSelected ? 'toggle' : ''")
     .form-container(class="h-full flex")
-      form.form-login(class="h-full w-[50%] duration-1000")
+      form.form-login(class="h-full w-full md:w-[50%] duration-1000 flex-col items-center justify-center py-16 ")
         h2(class="text-xl text-orange-600") Entrar com
+        input(type="text" placeholder="name" class="block my-10")
+        input(type="email" placeholder="email" class="block my-10")
+        button(type="button" class="text-orange-600") Entrar
 
-      form.form-register(class="h-full w-[50%] duration-1000")
-        h2(class="text-xl text-orange-600") Cadastrar
+        button(
+          type="button"
+          class="text-orange-600 underline block md:hidden"
+          @click="toggleForm()"
+        ) Ainda não tenho conta
+
+      form.form-register(class="h-full bg-orange-600 text-slate-900 md:bg-slate-900 md:text-orange-600 w-full md:w-[50%] duration-1000 flex-col items-center justify-center py-16 ")
+        h2(class="text-xl") Novo cadastro
+        input(type="text" placeholder="name" class="block my-10")
+        input(type="email" placeholder="email" class="block my-10")
+        button(type="button") Cadastrar
+
+        button(
+          type="button"
+          class="underline block md:hidden"
+          @click="toggleForm()"
+        ) Já tenho conta
 
     .form-overlay(class="w-full md:w-[50%] h-full absolute top-0 left-0 transition-all duration-1000 hidden z-10 md:block")
       .overlay-one(class="absolute w-full h-full bg-orange-600 duration-1000" v-if="formSelected")
