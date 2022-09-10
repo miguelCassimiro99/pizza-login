3bbba5e3e5ecdf65b844af9dde8396980baeec96
Miguel Cassimiro <miguel.cassimiro99@gmail.com>
2022-09-10 16:34:19 -0300

    fixed effects transitions

--- pages/login.vue
+++ pages/login.vue
@@ -1,17 +1,14 @@
 <template lang="pug">
 main(class="h-screen dark:bg-slate-900 flex justify-center items-center")
-  .login-container(
-    class="h-full w-full  md:h-[480px] md:w-[768px] rounded-md shadow-xl overflow-hidden border-slate-900 relative"
-    :class="formSelected ? 'toggle ' : ''"
-  )
+  .login-container(:class="formSelected ? 'toggle h-[480px] w-[768px] rounded-md shadow-xl overflow-hidden border-slate-900 relative' : ' h-[480px] w-[768px] rounded-md shadow-xl overflow-hidden border-slate-900 relative'")
     .form-container(class="h-full flex")
-      form.form-login(class="h-full w-full md:w-[50%] bg-slate-900 duration-1000")
+      form.form-login(class="h-full w-[50%] duration-1000")
         h2(class="text-xl text-orange-600") Entrar com
 
-      form.form-register(class="h-full w-[50%] bg-slate-900 duration-1000")
-        h2(class="text-xl text-orange-600") Não tenho conta
+      form.form-register(class="h-full w-[50%] duration-1000")
+        h2(class="text-xl text-orange-600") Cadastrar
 
-    .form-overlay(class="w-full md:w-[50%] absolute top-0 left-0 duration-1000 z-10 hidden md:block")
+    .form-overlay(class="w-full md:w-[50%] h-full absolute top-0 left-0 transition-all duration-1000 hidden z-10 md:block")
       .overlay-one(class="absolute w-full h-full bg-orange-600 duration-1000" v-if="formSelected")
         button(@click="toggleForm()") Já tenho conta
       .overlay-two(class="absolute w-full h-full bg-orange-600 duration-1000" v-else)
