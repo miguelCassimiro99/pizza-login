53ec6660425a763b68ab1695d216dd648e2769eb
Miguel Cassimiro <miguel.cassimiro99@gmail.com>
2022-09-10 21:42:01 -0300

    constructed input component

---
+++ components/BaseInput.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+section(class="flex flex-col")
+  label(class="text-left" v-if="label") {{ label }}
+  input(
+    class="py-2 px-3 text-base dark:text-slate-900"
+    type="text"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+    v-bind="$attrs"
+  )
+</template>
+<script setup>
+defineProps({
+  label: {
+    type: [String, Boolean],
+    default: false
+  },
+  modelValue: {
+    type: String,
+    default: "",
+  }
+})
+</script>
