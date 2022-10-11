935d5200d0f4cd54674133ee7c3e17fd7a120299
Miguel Cassimiro <miguels@umdoisesportes.com.br>
2022-10-10 23:13:56 -0300

    added server api

--- pages/notes.vue
+++ pages/notes.vue
@@ -1,9 +1,15 @@
 <template lang="pug">
 section(class="dark:bg-slate-900 h-screen w-full text-white absolute px-4 py-10")
-  header
+  header(class="flex justify-center items-center")
+    h1(class="mx-4") Hello {{ user?.email }}
     button(@click="signOut") Log out
-  div
-    h1 Hello notes
+  div(class="flex flex-col gap-4 mt-16")
+    div(
+      v-for="note in notes"
+      :key="note.id"
+      class="p-8 bg-gray-700 rounded"
+    ) {{ note.content }}
+
 </template>
 <script lang="ts" setup>
 const user = useSupabaseUser();
@@ -16,5 +22,10 @@ const signOut = async () => {
 
 watchEffect(() => {
   if(!user.value) navigateTo('/login')
+})
+
+const { data:notes } = await useFetch('/api/notes', {
+  key: `notes for ${user.value.id}`,
+  headers: useRequestHeaders(['cookie'])
 })
 </script>
